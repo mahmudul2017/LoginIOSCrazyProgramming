1e348b3dfd5adebfefba5f0092c3b08164a12a4e
Mahmudul2017 <mahmudulhasanshah@gmail.com>
2021-10-19 16:04:29 +0600

    Initial Commit

---
+++ LoginIOSCrazyProgramming/LoginIOSCrazyProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginIOSCrazyProgrammingApp.swift
+//  LoginIOSCrazyProgramming
+//
+//  Created by Jaman on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginIOSCrazyProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
